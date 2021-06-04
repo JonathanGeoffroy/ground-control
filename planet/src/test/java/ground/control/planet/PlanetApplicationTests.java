702ab17f64a433c69f4539b233555d935ec63b2f
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2021-06-04 08:55:18 +0200

    Initialize Planet project

---
+++ planet/src/test/java/ground/control/planet/PlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package ground.control.planet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
