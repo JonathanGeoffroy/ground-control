6e70655df59f1422a932a5e2868e78c0b550b2db
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2021-06-06 19:13:55 +0200

    OpenAPI 3 & Swagger

--- planet/src/main/java/ground/control/planet/controller/PlanetController.java
+++ planet/src/main/java/ground/control/planet/controller/PlanetController.java
@@ -4,8 +4,9 @@ import ground.control.planet.dto.CreatePlanetDTO;
 import ground.control.planet.dto.PlanetDTO;
 import ground.control.planet.dto.PlanetDetailsDTO;
 import ground.control.planet.entities.Planet;
-import ground.control.planet.exception.NotFoundEntityException;
 import ground.control.planet.service.PlanetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,8 +19,10 @@ import java.util.stream.Collectors;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-@RestController()
+
+@RestController
 @RequestMapping("/planet")
+@Tag(name = "Planet", description = "the Planet API")
 public class PlanetController {
     private final PlanetService service;
     private final ModelMapper modelMapper;
@@ -30,6 +33,7 @@ public class PlanetController {
         this.modelMapper = modelMapper;
     }
 
+    @Operation(summary = "Get all planets of the universe")
     @GetMapping(produces = "application/hal+json")
     public List<PlanetDTO> getAll() {
         return service.getAll()
@@ -41,21 +45,25 @@ public class PlanetController {
     }
 
 
+    @Operation(summary = "Get planet details")
     @GetMapping("/{id}")
-    public PlanetDetailsDTO getDetails(@PathVariable String id) throws NotFoundEntityException {
+    public PlanetDetailsDTO getDetails(@PathVariable String id) {
         var planet = service.findById(id);
         return modelMapper.map(planet, PlanetDetailsDTO.class);
     }
 
+    @Operation(summary = "Create a new planet")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+
     public PlanetDetailsDTO create(@Valid @RequestBody CreatePlanetDTO dto) {
         var planet = service.create(modelMapper.map(dto, Planet.class));
         return modelMapper.map(planet, PlanetDetailsDTO.class);
     }
 
+    @Operation(summary = "Delete an existing planet")
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable String id) throws NotFoundEntityException {
+    public void delete(@PathVariable String id) {
         service.deleteById(id);
     }
 }
--- planet/src/main/java/ground/control/planet/dto/PlanetDTO.java
+++ planet/src/main/java/ground/control/planet/dto/PlanetDTO.java
@@ -3,13 +3,17 @@ package ground.control.planet.dto;
 import lombok.*;
 import org.springframework.hateoas.RepresentationModel;
 
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = false)
 public class PlanetDTO extends RepresentationModel<PlanetDTO> {
+    @NotNull
     private String id;
+    @NotNull
     private String name;
     private Double size;
     private Double gravity;
