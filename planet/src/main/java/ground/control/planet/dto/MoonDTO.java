ed608d4e7f7a5b73891401dcb65f7c59383ff607
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2021-06-12 16:22:22 +0200

    FIX: mark id and name as mandatory fields

--- planet/src/main/java/ground/control/planet/dto/MoonDTO.java
+++ planet/src/main/java/ground/control/planet/dto/MoonDTO.java
@@ -5,11 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class MoonDTO {
+    @NotNull
     private String id;
+
+    @NotNull
     private String name;
 }
